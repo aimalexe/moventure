85b30febaa1e6a02e0c09e9118312b22b6755c97
Aimal Khan <aimalexe@gmail.com>
2024-06-23 14:22:14 +0500

    feat: User model validators and controllers updated according to ER diagram

--- backend/src/validators/reuseable-validators.js
+++ backend/src/validators/reuseable-validators.js
@@ -1,24 +1,10 @@
-import Joi from 'joi';
+import Joi from "joi";
 
-// name of a user.
-export const name = Joi
-    .string()
-    .min(3)
-    .max(50)
-    .trim()
-    .required();
-
-export const email = Joi
-    .string()
-    .email()
-    .min(5)
-    .max(255)
-    .trim()
-    .required();
-
-export const password = Joi
-    .string()
-    .min(8)
-    .max(255)
-    .trim()
-    .required();
+// Reusable validators
+export const email = Joi.string().email().min(5).max(255).required();
+export const password = Joi.string().min(8).max(255).required();
+export const firstName = Joi.string().min(3).max(50).required();
+export const lastName = Joi.string().min(3).max(50).optional();
+export const dateOfBirth = Joi.date().optional();
+export const phoneNumber = Joi.string().min(7).max(15).optional();
+export const address = Joi.string().min(5).max(255).optional();
